b5b501c55ec3ce28ac3e79abf193f17b72be96bb
MichaelJayFox <125122867+MichaelJayFox@users.noreply.github.com>
2023-04-09 14:11:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -7,10 +7,10 @@ template<typename T, int size>
 class TStack {
  private:
   int top;
-  T arr[100];
-  
+  T *arr;
+ 
  public:
-  TStack() : top(-1) {}
+  TStack() : top(-1) {array=new T[size];}
   void push(T value) {
       if (isFull()) {
           throw std::string("FULL!!!");
