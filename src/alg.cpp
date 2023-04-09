4c5d7c06816de60939ea6eb907116dd94215e452
MichaelJayFox <125122867+MichaelJayFox@users.noreply.github.com>
2023-04-09 15:55:11 +0300

    Revert "Update alg.cpp"

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,85 +1,118 @@
 // Copyright 2021 NNTU-CS
 #include <map>
 #include "tstack.h"
-#include <string>
-
-int pr(char x) {
-    if (x == '(') return 0;
-    else if (x == ')') return 1;
-    else if (x == '+' || x == '-') return 2;
-    else if (x == '*' || x == '/') return 3;
-    else return -1;
+int getPr(char d) {
+  switch (d) {
+    case '(':
+      return 0;
+      break;
+    case ')':
+      return 1;
+      break;
+    case '+':
+      return 2;
+      break;
+    case '-':
+      return 2;
+      break;
+    case '*':
+      return 3;
+      break;
+    case '/':
+      return 3;
+      break;
+    default:
+      return -1;
+      break;
+  }
+}
+int len(std::string str) {
+    char ch = str[0];
+    int res = 0;
+    while (ch != '\0')
+        ch = str[++res];
+    return res;
 }
 
-int num(char x) {
-    return (x >= '0' && x <= '9');
-}
-int op(char x) {
-    return (x == '+' || x == '-' \
-            || x == '*' || x == '/');
-}
-int conv(char x) {
-    char mas[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int toInt(char ch) {
+    char key[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     for (int i = 0; i < 10; i++) {
-        if (x == mas[i])
+        if (ch == key[i])
             return i;
     }
     return -1;
 }
+
 std::string infx2pstfx(std::string inf) {
-    TStack<char>  stack1;
-    std::string str = "";
-    for (int i = 0; i < inf.length(); i++) {
-        if (num(inf[i])) {
-            str += inf[i];
-            str += " ";
-        } else if (inf[i] == '(') {
-            stack1.push(inf[i]);
-        } else if (inf[i] == ')') {
-            while (!stack1.isEmpty() && stack1.get() != '(') {
-                str += stack1.get();
-                str += " ";
-                stack1.pop();
+    std::string rezult;
+    TStack<char, 100> stack11;
+    for (auto& d : inf) {
+        int priori = getPr(d);
+        if (priori == -1) {
+            rezult += d;
+        } else {
+            if (stack11.get() < priori || priori == 0 || stack11.isEmpty()) {
+                stack11.push(d);
+                stack11.push(d);
+            } else if (d == ')') {
+                char summa = stack11.get();
+                while (getPr(summa) >= priori) {
+                    rezult += summa;
+                    stack11.pop();
+                    summa = stack11.get();
+                }
+                stack11.pop();
+            } else {
+                char summa = stack11.get();
+                while (getPr(summa) >= priori) {
+                    rezult += summa;
+                    stack11.pop();
+                    summa = stack11.get();
+                }
+                stack11.push(d);
             }
-            stack1.pop();
-        } else if (op(inf[i])) {
-            while (!stack1.isEmpty() && pr(stack1.get()) \
-                   >= pr(inf[i])) {
-                str += stack1.get();
-                str += " ";
-                stack1.pop();
-            }
-            stack1.push(inf[i]);
         }
     }
-    while (!stack1.isEmpty()) {
-        str += stack1.get();
-        str += " ";
-        stack1.pop();
+    while (!stack11.isEmpty()) {
+        rezult += stack11.get();
+        stack11.pop();
     }
-    str.erase(str.length() - 1, 1);
-    return str;
+    rezult = spases(rezult);
+    return rezult;
 }
-int eval(std::string pref) {
-    TStack<int> stack1_;
-    for (int i = 0; i < pref.length(); i+=2) {
-        if (num(pref[i])) {
-            stack1_.push(conv(pref[i]));
-        } else if (op(pref[i])) {
-            int b = stack1_.get();
-            stack1_.pop();
-            int a = stack1_.get();
-            stack1_.pop();
-            if (pref[i] == '+') {
-                stack1_.push(a + b);
-            } else if (pref[i] == '-') {
-                stack1_.push(a - b);
-            } else if (pref[i] == '*') {
-                stack1_.push(a * b);
-            } else if (pref[i] == '/') {
-                stack1_.push(a / b);
+int vashislenia(const int& x, const int& y, const int& op) {
+    switch (op) {
+    case'+': return x + y;
+    case'-': return x - y;
+    case'*': return x * y;
+    case'/': return x / y;
+    default:
+        break;
+    }
+    return 0;
+}
+int eval(std::string post) {
+    TStack<int, 100> stack12;
+    std::string number = "";
+    for (int i = 0; i < post.size(); i++) {
+        if (getPr(post[i]) == -1) {Ц
+            if (post[i] == ' ') {
+                continue;
+            } else if (isdigit(post[i + 1])) {
+                number += post[i];
+                continue;
+            } else {
+                number += post[i];
+                stack12.push(atoi(number.c_str()));
+                number = "";
             }
+        } else {
+            int y = stack12.get();
+            stack12.pop();
+            int x = stack12.get();
+            stack12.pop();
+            stack12.push(vashislenia(x, y, post[i]));
         }
     }
-    return stack1_.get();
+    return stack12.get();
 }
